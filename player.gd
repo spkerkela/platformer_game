a23bda900b6c9e9f6877d97d4301f883ea897597
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2018-06-15 17:17:04 +0300

    add stomp move

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 const GRAVITY_VEC = Vector2(0, 900)
+const STOMP_VEC = Vector2(0, 5000)
 const FLOOR_NORMAL = Vector2(0, -1)
 const SLOPE_SLIDE_STOP = 25.0
 const MIN_ONAIR_TIME = 0.1
@@ -16,6 +17,7 @@ var onair_time = 0 #
 var on_floor = false
 var shoot_time=99999 #time since last shot
 var jumps = 0
+var stomping = false
 var anim=""
 
 #cache the sprite here for fast access (we will set scale to flip it often)
@@ -30,43 +32,53 @@ func _physics_process(delta):
 	### MOVEMENT ###
 
 	# Apply Gravity
-	linear_vel += delta * GRAVITY_VEC
+	if stomping:
+		linear_vel += delta * STOMP_VEC
+	else:
+		linear_vel += delta * GRAVITY_VEC
 	# Move and Slide
 	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
 	# Detect Floor
 	if is_on_floor():
 		jumps = 0
+		stomping = false
 		onair_time = 0
 
 	on_floor = onair_time < MIN_ONAIR_TIME
-
-	### CONTROL ###
-
-	# Horizontal Movement
 	var target_speed = 0
-	if Input.is_action_pressed("move_left"):
-		target_speed += -1
-	if Input.is_action_pressed("move_right"):
-		target_speed +=  1
+	### CONTROL ###
+	if stomping:
+		linear_vel.x = target_speed
+	else:
+		# Horizontal Movement
+		if Input.is_action_pressed("move_left"):
+			target_speed += -1
+		if Input.is_action_pressed("move_right"):
+			target_speed +=  1
+	
+		target_speed *= WALK_SPEED
+		linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
+	
+		# Jumping
+		if (on_floor or jumps < MAX_JUMPS) and Input.is_action_just_pressed("jump"):
+			linear_vel.y = -JUMP_SPEED
+			jumps += 1
+			$sound_jump.play()
+	
+		# Shooting
+		if Input.is_action_just_pressed("shoot"):
+			var bullet = preload("res://bullet.tscn").instance()
+			bullet.position = $sprite/bullet_shoot.global_position #use node for shoot position
+			bullet.linear_velocity = Vector2(sprite.scale.x * BULLET_VELOCITY, 0)
+			bullet.add_collision_exception_with(self) # don't want player to collide with bullet
+			get_parent().add_child(bullet) #don't want bullet to move with me, so add it as child of parent
+			$sound_shoot.play()
+			shoot_time = 0
+		
+		if not on_floor and Input.is_action_just_pressed("move_down"):
+			stomping = true
 
-	target_speed *= WALK_SPEED
-	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
-
-	# Jumping
-	if (on_floor or jumps < MAX_JUMPS) and Input.is_action_just_pressed("jump"):
-		linear_vel.y = -JUMP_SPEED
-		jumps += 1
-		$sound_jump.play()
-
-	# Shooting
-	if Input.is_action_just_pressed("shoot"):
-		var bullet = preload("res://bullet.tscn").instance()
-		bullet.position = $sprite/bullet_shoot.global_position #use node for shoot position
-		bullet.linear_velocity = Vector2(sprite.scale.x * BULLET_VELOCITY, 0)
-		bullet.add_collision_exception_with(self) # don't want player to collide with bullet
-		get_parent().add_child(bullet) #don't want bullet to move with me, so add it as child of parent
-		$sound_shoot.play()
-		shoot_time = 0
+		
 
 	### ANIMATION ###
 
